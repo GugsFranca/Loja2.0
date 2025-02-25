8d9740efba4141ed45f028baa6ba2cd9ef9992be
Gustavo de França <gustavonota20@hotmail.com>
2025-02-25 15:47:23 -0300

    Removendo import não utilizado

--- CartService/src/main/java/loja/cartservice/entity/CartEntity.java
+++ CartService/src/main/java/loja/cartservice/entity/CartEntity.java
@@ -3,11 +3,9 @@ package loja.cartservice.entity;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @Setter
